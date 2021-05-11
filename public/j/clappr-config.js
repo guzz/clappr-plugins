bd700753b35c7fcd93159f5c0071194fa96131f7
Gustavo Junqueira <gsotero@gmail.com>
2021-05-11 13:22:44 -0300

    Update watermark plugin

--- public/j/clappr-config.js
+++ public/j/clappr-config.js
@@ -6,10 +6,19 @@ player = new Clappr.Player({
   source: urlParams.src || 'http://clappr.io/highline.mp4',
   poster: urlParams.poster || 'http://clappr.io/poster.png',
   mute: true,
-  autoPlay: true,
   height: 360,
   width: 640,
   plugins: plugins,
+  watermark: {
+    url: 'https://v-clappr-player.netlify.app/logo.svg',
+    link: 'https://v-clappr-player.netlify.app',
+    opacity: 0.8,
+    behavior: 'discrete',
+    nudge: {
+      y: 5,
+      x: -24
+    }
+  }
 })
 
 player.attachTo(playerElement)
--- src/plugins/watermark/watermark.js
+++ src/plugins/watermark/watermark.js
@@ -7,6 +7,22 @@ import { Events, UIContainerPlugin, template } from '@clappr/core'
 import watermarkHTML from './public/watermark.html'
 import './public/watermark.scss'
 
+const OPTIONS_DEFAULT = {
+  position: 'bottom-right',
+  opacity: 1,
+  behavior: 'always-fixed', // discrete
+  discreteDelay: 3000,
+  discreteOpacity: 0.2,
+  onControls: {
+    x: 0,
+    y: 48
+  },
+  nudge: {
+    x: 0,
+    y: 0
+  }
+}
+
 export default class WaterMarkPlugin extends UIContainerPlugin {
   get name() { return 'watermark' }
   get supportedVersion() { return { min: CLAPPR_CORE_VERSION } }
@@ -19,32 +35,116 @@ export default class WaterMarkPlugin extends UIContainerPlugin {
 
   bindEvents() {
     this.listenTo(this.container, Events.CONTAINER_PLAY, this.onPlay)
+    this.listenTo(this.container, Events.CONTAINER_PAUSE, this.onPause)
     this.listenTo(this.container, Events.CONTAINER_STOP, this.onStop)
+    this.listenTo(this.container, Events.CONTAINER_ENDED, this.onStop)
     this.listenTo(this.container, Events.CONTAINER_OPTIONS_CHANGE, this.configure)
+    this.listenTo(this.container, Events.CONTAINER_MEDIACONTROL_HIDE, this.resetBehavior)
+    this.listenTo(this.container, Events.CONTAINER_MEDIACONTROL_SHOW, this.executeBehavior)
   }
 
   configure() {
-    this.position = this.options.position || 'bottom-right'
-    if (this.options.watermark) {
-      this.imageUrl = this.options.watermark
-      this.imageLink = this.options.watermarkLink
+    const watermarkOptions = this.getOptions(this.options)
+    if (watermarkOptions) {
+      this.position = watermarkOptions.position
+      this.imageUrl = watermarkOptions.url
+      this.imageLink = watermarkOptions.link
+      this.opacity = watermarkOptions.opacity
+      this.behavior = watermarkOptions.behavior
+      this.nudge = watermarkOptions.nudge
+      this.discreteDelay = watermarkOptions.discreteDelay
+      this.discreteOpacity = watermarkOptions.discreteOpacity
+      this.onControls = watermarkOptions.onControls
       this.render()
     } else { this.$el.remove() }
 
   }
 
+  getOptions(options) {
+    if (typeof options.watermark === 'object' && options.watermark.url) {
+      return Object.assign({}, OPTIONS_DEFAULT, options.watermark)
+    } else if (typeof options.watermark === 'string') {
+      return Object.assign(
+        {},
+        OPTIONS_DEFAULT,
+        {
+          url: options.watermark,
+          link: options.watermarkLink,
+          position: options.position
+        }
+      )
+    } else {
+      return null
+    }
+  }
+
+  nudgeTransform(nudge) {
+    const transformedNudge = { ...nudge }
+    if (this.position.includes('bottom')) {
+      transformedNudge.y = - transformedNudge.y
+    }
+    if (this.position.includes('right')) {
+      transformedNudge.x = - transformedNudge.x
+    }
+    transformedNudge.x = transformedNudge.x || 0
+    transformedNudge.y = transformedNudge.y || 0
+    return transformedNudge
+  }
+
+  executeBehavior() {
+    if (!this.behavior.includes('fixed')) {
+      const nudge = {
+        x: this.onControls.x + this.nudge.x,
+        y: this.onControls.y + this.nudge.y
+      }
+      const transformedNudge = this.nudgeTransform(nudge)
+      this.$el.find('.clappr-watermark')[0].style.transform = `translate(${transformedNudge.x}px, ${transformedNudge.y}px)`
+      this.$el.find('.clappr-watermark')[0].style.zIndex = 100000
+    }
+    if (this.behavior.includes('discrete') && this.timeout) {
+      this.$el.find('.clappr-watermark')[0].style.opacity = this.opacity
+      clearTimeout(this.timeout)
+    }
+  }
+  
+  resetBehavior() {
+    if (!this.behavior.includes('fixed')) {
+      const transformedNudge = this.nudgeTransform(this.nudge)
+      this.$el.find('.clappr-watermark')[0].style.transform = `translate(${transformedNudge.x}px, ${transformedNudge.y}px)`
+    }
+    if (this.behavior.includes('discrete')) {
+      this.timeout = setTimeout(() => {
+        if (!this.paused) {
+          this.$el.find('.clappr-watermark')[0].style.opacity = this.discreteOpacity
+        }
+      }, this.discreteDelay)
+    }
+  }
+
   onPlay() {
+    this.paused = false
     if (!this.hidden)
       this.$el.show()
   }
 
+  onPause() {
+    this.paused = true
+  }
+
   onStop() {
+    console.log('stop')
     this.$el.hide()
   }
 
   render() {
     this.$el.hide()
-    const templateOptions = { position: this.position, imageUrl: this.imageUrl, imageLink: this.imageLink }
+    const templateOptions = {
+      position: this.position,
+      imageUrl: this.imageUrl,
+      imageLink: this.imageLink,
+      opacity: this.opacity,
+      nudge: this.nudgeTransform(this.nudge)
+    }
     this.$el.html(this.template(templateOptions))
     this.container.$el.append(this.$el)
     return this
